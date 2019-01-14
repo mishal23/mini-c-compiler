6887875a780dbcca29ce99bbe4c060ea69d3d261
Mishal Shah <shahmishal1998@gmail.com>
2019-01-14 22:09:13 +0530

    updated scanner to take input from bash script

--- run.sh
+++ run.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
+function run() {
+	lex scanner1.l && gcc lex.yy.c
+	local total_testcases="$1"
+	echo "Running: $total_testcases"
+	local start=1
+	while [ $start -le $total_testcases ]
+	do
+		echo `\n\n`
+		echo Running TestCase $start
+		local filename="tests/test"$start".c"
+		./a.out $filename
+		((start++))
+	done
+}
 
-lex scanner1.l && gcc lex.yy.c && ./a.out
+number_of_files=`ls -l ./tests/ | egrep -c '^-'`
+run $number_of_files
