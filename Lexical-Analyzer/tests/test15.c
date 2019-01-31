dfc12c70fafdf0b79a7a6aca7f89a190941dd564
Mishal Shah <shahmishal1998@gmail.com>
2019-01-31 11:44:30 +0530

    parser grammar for function declaration done

--- Lexical-Analyzer/tests/test15.c
+++ Lexical-Analyzer/tests/test15.c
@@ -1,1 +1,21 @@
-float int i = 0;
+#include <stdio.h>
+
+int myfunc1()
+{
+  int a;
+}
+
+int myfunc2(int a, int b,char e
+{
+   int c,d;
+}
+
+int myfunc3(int a, char b[])
+{
+  int e;
+}
+
+int main()
+{
+	int h[100];
+}
