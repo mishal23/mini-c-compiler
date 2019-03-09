99d7cb71bc37c8809bc77e398c7c9482716e1577
Pavan Vachhani <vachhanihpavan@gmail.com>
2019-03-09 20:15:26 +0530

    Check function name and ID name match

--- Semantic Analysis/tests/test16.c
+++ Semantic Analysis/tests/test16.c
@@ -1,25 +1,16 @@
 #include<stdio.h>
-
-void square(int a)
+char square(int a)
 {
     int b;
 	return b;
 }
 
-struct abc
-{
-   int a;
-   char b;
-};
-
 int main()
 {
-	struct abc A;
-	A.a = 2;
 	int num = 2;
-	int num2 = square(num);
+	int square;
 	
-	printf("Square of %d is %d", num, num2);
+	printf("Square of %d is %d", num, square);
 
 	return 0;
 }
